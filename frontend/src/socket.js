9908ec8d60e522f7c45d23547608711cecfdf8e6
Alok <108712328+Krishan-Kant123@users.noreply.github.com>
2025-06-04 13:13:17 +0530

    Update socket.js

--- frontend/src/socket.js
+++ frontend/src/socket.js
@@ -1,6 +1,7 @@
 import { io } from "socket.io-client";
 
-const SOCKET_SERVER_URL = process.env.REACT_APP_SOCKET_SERVER_URL || "http://localhost:5000";
+// const SOCKET_SERVER_URL = process.env.REACT_APP_SOCKET_SERVER_URL || "http://localhost:5000";
+const SOCKET_SERVER_URL ="https://tic-tac-toe-multiplayer-backend-lv94.onrender.com"
 
 const socket = io(SOCKET_SERVER_URL, {
 //   autoConnect: false,
